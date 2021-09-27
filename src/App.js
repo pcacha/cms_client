71fd8b1e0ee1acbe52b009686747ee4a5381e202
Pavel Čácha <pavelcacha@email.cz>
2021-09-28 00:15:39 +0200

    basic infrastructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
-function App() {
-  return (
-   null
-  );
+import React, {Component} from 'react';
+import Layout from "./components/Layout"
+import {Route, Switch, Redirect} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import {connect} from "react-redux";
+import ArticlesPage from "./pages/ArticlesPage";
+import NewArticlePage from "./pages/NewArticlePage";
+import ProfilePage from "./pages/ProfilePage";
+import MyArticlesPage from "./pages/MyArticlesPage";
+import AssignedReviewsPage from "./pages/AssignedReviewsPage";
+import ReviewPage from "./pages/ReviewPage";
+import MyReviewsPage from "./pages/MyReviewsPage";
+import ArticleManagerPage from "./pages/ArticleManagerPage";
+import UserManagerPage from "./pages/UserManagerPage";
+import ReviewersOverviewPage from "./pages/ReviewersOverviewPage";
+import ReviewsOverviewPage from "./pages/ReviewsOverviewPage";
+import DecidePage from "./pages/DecidePage";
+
+class App extends Component {
+
+    render() {
+        const {user} = this.props;
+
+        let routes = (
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/login" component={LoginPage}/>
+                <Route exact path="/registrace" component={RegisterPage}/>
+                <Route exact path="/clanky" component={ArticlesPage}/>
+                {user.isLoggedIn && <Route exact path="/mojeUdaje" component={ProfilePage} />}
+                {user.isAuthor && <Route exact path="/publikovat" component={NewArticlePage} />}
+                {user.isAuthor && <Route exact path="/publikovat/:id" component={NewArticlePage} />}
+                {user.isAuthor && <Route exact path="/mojeClanky" component={MyArticlesPage} />}
+                {user.isReviewer && <Route exact path="/recenzovat" component={AssignedReviewsPage} />}
+                {user.isReviewer && <Route exact path="/recenzovat/:id" component={ReviewPage} />}
+                {user.isReviewer && <Route exact path="/mojeRecenze" component={MyReviewsPage} />}
+                {user.isAdmin && <Route exact path="/spravceClanku" component={ArticleManagerPage} />}
+                {user.isAdmin && <Route exact path="/spravceUzivatelu" component={UserManagerPage} />}
+                {user.isAdmin && <Route exact path="/spravceClanku/recenzenti/:id" component={ReviewersOverviewPage} />}
+                {user.isAdmin && <Route exact path="/spravceClanku/recenze/:id" component={ReviewsOverviewPage} />}
+                {user.isAdmin && <Route exact path="/spravceClanku/rozhodnout/:id" component={DecidePage} />}
+                <Redirect to="/"/>
+            </Switch>
+        );
+
+        return (
+            <Layout>
+                {routes}
+            </Layout>
+        );
+    }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+    return {
+        user: state,
+    };
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import store from "./store/store";
+import {Provider} from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
